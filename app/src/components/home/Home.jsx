df9196af1d389cf87a72a6b84f1d5635cb9e1b8a
LuisPadillaAR <luis0907p@gmail.com>
2024-06-09 00:36:17 -0500

    v4

--- app/src/components/home/Home.jsx
+++ app/src/components/home/Home.jsx
@@ -19,12 +19,12 @@ export default function Home() {
                 <Grid item>
                     <Grid container flexDirection={'column'} gap={2}>
                         <Grid item>
-                            <Container sx={{boxShadow: 4, padding: 2, borderRadius: 2}}>
+                            <Container sx={{boxShadow: 4, padding: 2, borderRadius: 2, color: 'orangered', backgroundColor: '#000'}}>
                                 <Typography variant="h5" component={'p'}>Esto es una aplicación para crear notas. Hecha con javascript, utilizando la biblioteca de React. Está conectada a una base de datos en la nube, por tanto hago uso de API REST para enviar y recibir datos. La base de datos que uso es MongoDB porque usa json y se adapta a este mini proyecto. :D</Typography>
                             </Container>
                         </Grid>
                         <Grid item>
-                            <Container sx={{boxShadow: 4, padding: 2, borderRadius: 2}}>
+                            <Container sx={{boxShadow: 4, padding: 2, borderRadius: 2, color: 'orangered', backgroundColor: '#000'}}>
                                 <Typography variant="h5" component={'p'}>El repositorio de este proyecto se encuentra aquí: <Link variant="h5" href="https://github.com/LuisPadillaAR/winnotes.git">https://github.com/LuisPadillaAR/winnotes.git</Link>
                                 </Typography>
                             </Container>
