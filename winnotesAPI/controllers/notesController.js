de7d09c0a316cdc0c02018316e88924e384fd8cb
LuisPadillaAR <luis0907p@gmail.com>
2024-06-08 23:12:35 -0500

    sad

--- winnotesAPI/controllers/notesController.js
+++ winnotesAPI/controllers/notesController.js
@@ -3,26 +3,26 @@ import NotesModel from '../model/notesModel.js';
 export const getAll = async (req, res) => {
     try {
         const data = await NotesModel.find()
-    
-        if(!data){
-            return res.status(404).json({msg: 'no encontrado'});
+
+        if (!data) {
+            return res.status(404).json({ msg: 'no encontrado' });
         }
         return res.status(200).json(data)
     } catch (error) {
-        return res.status(500).json({error: error})
+        return res.status(500).json({ error: error })
     }
 }
 
 export const getId = async (req, res) => {
     try {
         const data = await NotesModel.findById(req.params.id)
-    
-        if(!data){
-            return res.status(404).json({msg: 'no encontrado'});
+
+        if (!data) {
+            return res.status(404).json({ msg: 'no encontrado' });
         }
         return res.status(200).json(data)
     } catch (error) {
-        return res.status(500).json({error: error, msg: 'raroooo'})
+        return res.status(500).json({ error: error, msg: 'raroooo' })
     }
 }
 
